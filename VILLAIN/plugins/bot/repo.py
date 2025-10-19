9a741dfcfaf9615afadbef6cfd0bcaaf0c6cee29
aditya88402 <aditya266015@gmail.com>
2025-10-19 19:40:06 +0530

    Update repo.py

--- VILLAIN/plugins/bot/repo.py
+++ VILLAIN/plugins/bot/repo.py
@@ -60,7 +60,7 @@ async def repo(_, message):
             list_of_users += f"{count}. [{user['login']}]({user['html_url']})\n"
             count += 1
 
-        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://t.me/iamakii73) | [𝖦𝖱𝖮𝖴𝖯](https://t.me/+o7_uPKxtL5hkY2Q1)
+        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://t.me/I_VAMPIRE_KING) | [𝖦𝖱𝖮𝖴𝖯](https://t.me/+o7_uPKxtL5hkY2Q1)
 | 𝖢𝖮𝖭𝖳𝖱𝖨𝖡𝖴𝖳𝖮𝖱𝖲 |
 ----------------
 {list_of_users}"""
